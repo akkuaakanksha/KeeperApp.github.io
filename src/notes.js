675cace733432440e52df1f31a820d4796af50e4
Aakanksha <75885261+akkuaakanksha@users.noreply.github.com>
2021-08-06 20:42:27 +0530

    Keeper App..

---
+++ src/notes.js
@@ -0,0 +1,21 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+
+    content:
+      "Q. how many programmer does it takes to change a light bulb? A. None"
+  },
+  {
+    key: 2,
+    title: "Array",
+    content: "Q. xyx? Ans. none"
+  },
+
+  {
+    key: 3,
+    title: "Loops",
+    content: "  Q. xyz?  Ans. none"
+  }
+];
+export default notes;
